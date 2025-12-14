83f3f2a8440821ac026f606a4a6dcadf38f46e84
rpw1134 <ryanw1134@gmail.com>
2025-12-13 22:14:13 -0500

    finishing up SVG stuff

--- src/Components/svgs/FastAPI.tsx
+++ src/Components/svgs/FastAPI.tsx
@@ -1,11 +1,13 @@
 export default function FastAPISVG() {
   return (
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="12" cy="12" r="10" fill="#009688" />
-      <path
-        d="M13 4L10 11h3l-3 9 6-10h-3.5L16 4h-3z"
-        fill="white"
-      />
+    <svg
+      fill="#009688"
+      role="img"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>FastAPI</title>
+      <path d="M12 .0387C5.3729.0384.0003 5.3931 0 11.9988c-.001 6.6066 5.372 11.9628 12 11.9625 6.628.0003 12.001-5.3559 12-11.9625-.0003-6.6057-5.3729-11.9604-12-11.96m-.829 5.4153h7.55l-7.5805 5.3284h5.1828L5.279 18.5436q2.9466-6.5444 5.892-13.0896" />
     </svg>
   );
 }
